1efae04bea71749da737f474bdb646715658ec54
ledxdd <oVladid11@gmail.com>
2026-02-22 23:04:26 +0300

    fix .gitignore

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,16 @@ public class Main {
         Battle b = new Battle();
         Pokemon p1 = new Blissey("Чужой", 1);
         Pokemon p2 = new Clauncher("Хищник", 1);
+        Pokemon p3 = new Chansey("Арсений", 1);
+        Pokemon p4 = new Clawitzer("Димас", 1);
+        Pokemon p5 = new Dhelmise("Антон", 1);
+        Pokemon p6 = new Happiny("Владос", 1);
         b.addAlly(p1);
+        b.addAlly(p3);
+        b.addAlly(p5);
         b.addFoe(p2);
+        b.addFoe(p4);
+        b.addFoe(p6);
         b.go();
     }
 }
